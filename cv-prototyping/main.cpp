d58e3b1653a8aad2b7b598775ce095db54d1a455
311Volt <113amper@gmail.com>
2022-10-16 13:49:10 +0200

    add perspective correction

--- cv-prototyping/main.cpp
+++ cv-prototyping/main.cpp
@@ -34,20 +34,33 @@ cv::Mat squareDist(cv::Mat source, cv::Vec3f vec)
 	return out;
 }
 
+cv::Mat getPerspectiveCorrectionMatrix(const std::vector<cv::Point>& points)
+{
+	std::vector<cv::Point> dstPoints = {
+		{0, 433},
+		{250, 866},
+		{750, 866},
+		{1000, 433},
+		{750, 0},
+		{250, 0}
+	};
+	return cv::findHomography(points, dstPoints);
+}
+
 std::vector<cv::Point> findBoardVertices(cv::Mat thres)
 {
 	std::vector<std::vector<cv::Point>> contours;
 	std::vector<cv::Point> hex;
 	cv::Mat thresCont;
 	cv::findContours(thres, contours, cv::RETR_TREE, cv::CHAIN_APPROX_NONE);
-	cv::approxPolyDP(contours[0], hex, cv::arcLength(contours[0], true)*0.05, true);
+	cv::approxPolyDP(contours[0], hex, cv::arcLength(contours[0], true)*0.02, true);
 
 	return hex;
 }
 
 int main()
 {
-	auto src = cv::imread("resources/sampleGlare.jpg");
+	auto src = cv::imread("resources/sampleGlare2.jpg");
 	cv::Mat crcb = convertToCrCb(src);
 	cv::Mat sq = squareDist(crcb, SEA_COLOR_YCBCR_6500K);
 	//cv::Mat sq = squareDist(crcb, SEA_COLOR_YCBCR_3400K);
@@ -56,12 +69,17 @@ int main()
 	cv::threshold(sq, thres, 20, 255, cv::THRESH_BINARY_INV);
 
 	std::vector<std::vector<cv::Point>> hex = {findBoardVertices(thres)};
-	cv::drawContours(src, hex, 0, {255,0,255}, 7);
+	//cv::drawContours(src, hex, 0, {255,0,255}, 7);
+	
+	cv::Mat corr = getPerspectiveCorrectionMatrix(hex[0]);
+	cv::Mat final;
+	cv::warpPerspective(src, final, corr, {1000, 866});
 
 	cv::imshow("Source", src);
 	cv::imshow("CrCb", crcb);
 	cv::imshow("sqDiff(CrCb, sea color)", sq);
 	cv::imshow("Threshold", thres);
+	cv::imshow("Warped", final);
 	
 
 	cv::waitKey();

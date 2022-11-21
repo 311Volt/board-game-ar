940607d77e523169fd63e374500b9cbb97b91135
WonderWoman <74731481+WonderWoman2020@users.noreply.github.com>
2022-11-21 04:14:36 +0100

    Cards detection, plus first attempt to recognize them (dirty)

--- cv-prototyping/src/main.cpp
+++ cv-prototyping/src/main.cpp
@@ -53,9 +53,107 @@ double GetTime()
 	return 1e-9 * std::chrono::duration_cast<std::chrono::nanoseconds>(t1-t0).count();
 }
 
+
+//----- cards detection -----//
+
+cv::Mat scaleImage(cv::Mat image, float scale)
+{
+	cv::Mat scaled;
+	cv::resize(image, scaled, cv::Size(image.size().width * scale, image.size().height * scale), cv::INTER_AREA);
+	return scaled;
+}
+
+cv::Mat detectWhiteFields(cv::Mat image)
+{
+	cv::Mat hsvCardsPhoto;
+	cv::cvtColor(image, hsvCardsPhoto, cv::COLOR_BGR2HSV);
+	cv::imshow("hsv", hsvCardsPhoto);
+
+	cv::Mat whiteFieldsofPhoto;
+	cv::inRange(hsvCardsPhoto, cv::Vec3b(0, 0, 110), cv::Vec3b(179, 30, 255), whiteFieldsofPhoto);
+	cv::imshow("White fields of photo", whiteFieldsofPhoto);
+
+	return whiteFieldsofPhoto;
+}
+
+std::vector<std::vector<cv::Point>> filterContoursForBigQuadrangles(std::vector<std::vector<cv::Point>> contours)
+{
+	std::vector<std::vector<cv::Point>> filteredContours;
+	for (auto c : contours)
+	{
+		if (cv::contourArea(c) > 1000)
+		{
+			auto peri = cv::arcLength(c, true);
+			std::vector<cv::Point> approx;
+			cv::approxPolyDP(c, approx, peri * 0.025, true);
+			if (approx.size() == 4)
+			{
+				filteredContours.push_back(c);
+			}
+		}
+	}
+	return filteredContours;
+}
+
+cv::Mat maskAndCropOutCard(cv::Mat image, std::vector<cv::Point> c)
+{
+	// masking out single card (or glued, by contours, few cards)
+	cv::Mat mask = cv::Mat::zeros(image.size(), image.type());
+	std::vector<std::vector<cv::Point>> cs = { c };
+	cv::drawContours(mask, cs, -1, cv::Scalar(255, 255, 255), -1);
+	cv::bitwise_and(mask, image, mask);
+
+	//cropping out card
+	cv::Rect cardBoundaries = cv::boundingRect(c);
+	cv::Mat card = mask(cardBoundaries);
+	return card;
+}
+
+std::vector<cv::Mat> detectCards(cv::Mat image)
+{
+	
+	cv::Mat whiteFieldsofPhoto = detectWhiteFields(image);
+	std::vector<std::vector<cv::Point>> contours;
+	cv::findContours(whiteFieldsofPhoto, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
+
+	std::vector<std::vector<cv::Point>> filteredContours = filterContoursForBigQuadrangles(contours);
+
+	// show one mask for all cards (for debugging only, to check if all cards where detected)
+	cv::Mat maskFiltered = cv::Mat::zeros(image.size(), image.type());
+	cv::drawContours(maskFiltered, filteredContours, -1, cv::Scalar(255, 255, 255), -1);
+	cv::imshow("Filtered mask", maskFiltered);
+	cv::Mat maskedCards;
+	cv::bitwise_and(image, maskFiltered, maskedCards);
+	cv::imshow("Masked Cards", maskedCards);
+
+
+	std::vector<cv::Mat> croppedOutCards;
+	for (auto c : filteredContours)
+	{
+		cv::Mat card = maskAndCropOutCard(image, c);
+		croppedOutCards.push_back(card);
+	}
+
+	return croppedOutCards;
+}
+
+// PS. the cards are downscaled here too
+std::vector<cv::Mat> readTemplateCards()
+{
+	std::vector<cv::Mat> templateCards;
+	templateCards.push_back(scaleImage(cv::imread("resources/cards/point_cathedral.jpg", cv::IMREAD_GRAYSCALE), 0.3));
+	templateCards.push_back(scaleImage(cv::imread("resources/cards/point_cityHall.jpg", cv::IMREAD_GRAYSCALE), 0.3));
+	templateCards.push_back(scaleImage(cv::imread("resources/cards/point_library.jpg", cv::IMREAD_GRAYSCALE), 0.3));
+	templateCards.push_back(scaleImage(cv::imread("resources/cards/point_marketplace.jpg", cv::IMREAD_GRAYSCALE), 0.3));
+	templateCards.push_back(scaleImage(cv::imread("resources/cards/point_university.jpg", cv::IMREAD_GRAYSCALE), 0.3));
+	return templateCards;
+}
+
+
+
 int main()
 {
-	auto src = cv::imread("resources/sample1.jpg");
+	/*auto src = cv::imread("resources/sample1.jpg");
 	CatanBoardDetector detector {SEA_COLOR_YCBCR_6500K};
 
 	auto warpedOpt = detector.findBoard(src);
@@ -90,5 +188,86 @@ int main()
 	DrawRoads(warped, boardInfo);
 	
 	cv::imshow("Warped board", warped);
+	cv::waitKey();*/
+
+	//----- cards detection -----//
+	cv::Mat srcCardsPhoto = cv::imread("resources/sampleBoardWithCards.jpg");
+	srcCardsPhoto = scaleImage(srcCardsPhoto, 0.3);
+	cv::imshow("Src Cards Photo", srcCardsPhoto);
+
+	std::vector<cv::Mat> croppedOutCards = detectCards(srcCardsPhoto);
+
+	// show every single cropped out card
+	int i = 1;
+	for (auto card : croppedOutCards)
+	{
+		cv::imshow("Card " + std::to_string(i), card);
+		i++;
+	}
+
+	std::vector<cv::Mat> templateCards = readTemplateCards();
+	// show templates
+	int z = 1;
+	for (auto temp : templateCards)
+	{
+		//cv::imshow("Temp " + std::to_string(j), scaleImage(temp, 0.3));
+		cv::imshow("Temp " + std::to_string(z), temp);
+		z++;
+	}
+
+	/*auto orb = cv::ORB::create(); // ORB
+	auto matcher = cv::BFMatcher::create();
+	std::vector<std::vector<cv::KeyPoint>> keypointsTemplates;
+	std::vector<cv::Mat> descriptorsTemplates;
+
+	// prepare orb thingies for all template cards
+	int j = 1;
+	for (auto temp : templateCards)
+	{
+		std::vector<cv::KeyPoint> keypointsTemp;
+		cv::Mat descriptorsTemp;
+		orb->detectAndCompute(temp, NULL, keypointsTemp, descriptorsTemp);
+		keypointsTemplates.push_back(keypointsTemp);
+		descriptorsTemplates.push_back(descriptorsTemp);
+		//cv::imshow("Temp " + std::to_string(j), scaleImage(temp, 0.3));
+		cv::imshow("Temp " + std::to_string(j), temp);
+		j++;
+		
+	}
+
+	// trying to do recognize the cards with orby
+	// comparing each detected card with all template cards
+	int k = 1;
+	for (auto card : croppedOutCards)
+	{
+		cv::Mat grayCard;
+		cv::cvtColor(card, grayCard, cv::COLOR_BGR2GRAY);
+		std::vector<cv::KeyPoint> keypointsCard;
+		cv::Mat descriptorsCard;
+		orb->detectAndCompute(grayCard, NULL, keypointsCard, descriptorsCard);
+		for (int a = 0; a < templateCards.size(); a++)
+		{
+			std::vector<cv::DMatch> matches;
+			matcher->match(descriptorsTemplates.at(a), descriptorsCard, matches);
+			// trying to get the best matches, which means the ones with the least distance
+			std::vector<cv::DMatch> goodMatches;
+			std::sort(matches.begin(), matches.end(), [](const auto& first, const auto& second) {return first.distance < second.distance; });
+			for (int p=0; p<30; p++)
+			{
+				if(matches.at(p).distance < 300)
+					goodMatches.push_back(matches.at(p));
+			}
+			// saving comparison images with good matches detected to analyze them
+			cv::Mat comparisonImage;
+			cv::drawMatches(templateCards.at(a), keypointsTemplates.at(a), grayCard, keypointsCard, goodMatches, comparisonImage);
+			//cv::imshow("Match " + std::to_string(a), comparisonImage);
+			cv::imwrite("resources/test_cards/match_" + std::to_string(k) + ".jpg", comparisonImage);
+			k++;
+		}
+	}*/
+
 	cv::waitKey();
+
+
+
 }
